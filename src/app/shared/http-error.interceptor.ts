afed7c6585e407b9252f9ac7357d3a46e612b6c8
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-17 22:50:55 +0100

    added doc for the httpErrorInterceptor

--- src/app/shared/http-error.interceptor.ts
+++ src/app/shared/http-error.interceptor.ts
@@ -16,6 +16,20 @@ export class HttpErrorInterceptor implements HttpInterceptor {
 
   constructor(public dialog: MatDialog) { }
 
+
+  /**
+   * Intercepts HTTP requests and handles errors.
+   *
+   * This method intercepts all outgoing HTTP requests and applies error handling. If the request is for
+   * login or signup and returns a 400 error, it rethrows the error. For all other errors, it extracts
+   * and formats the error message using `getErrorMessage`, displays it using a DialogErrorComponent, 
+   * and then rethrows the error. This centralized error handling is useful for displaying consistent 
+   * error messages across the application.
+   *
+   * @param req - The outgoing HttpRequest object.
+   * @param next - The HttpHandler for the next interceptor in the chain.
+   * @returns An Observable that emits the HttpEvent stream.
+   */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
@@ -31,6 +45,19 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     );
   }
 
+
+  /**
+   * Generates an appropriate error message based on the HTTP error status.
+   *
+   * This private method interprets the HTTP error response and returns a user-friendly error message.
+   * It handles various HTTP status codes such as 403 (Forbidden), 400 (Bad Request), and 401 (Unauthorized),
+   * providing specific messages for each. For all other unexpected error statuses, it returns a generic 
+   * error message. This method is typically used in conjunction with error handling in HTTP request 
+   * interceptors or similar contexts.
+   *
+   * @param error - The HttpErrorResponse object containing the error details.
+   * @returns A string representing a user-friendly error message.
+   */
   private getErrorMessage(error: HttpErrorResponse): string {
     switch (error.status) {
       case 403:
